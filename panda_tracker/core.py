d3c163250f80e05b4979d1d1d02e0f62b8aa41c1
Andrey Petrov <nomad@yandex-team.ru>
2016-04-28 20:10:45 +0300

    New route decorator.

--- panda_tracker/core.py
+++ panda_tracker/core.py
@@ -1,3 +1,6 @@
+from functools import wraps
+from werkzeug.wrappers import Response
+from flask import jsonify
 from flask.ext.pymongo import PyMongo
 from flask_jwt import JWT
 
@@ -18,3 +21,38 @@ class ApplicationError(Exception):
 class InputError(ApplicationError):
     def __init__(self, description):
         super().__init__('InputError', description, 400)
+
+
+def route(bp, *args, **kwargs):
+    """Almost like standard `route`, but with modified defaults and adds few
+    nice features. For example any value returned by decorated view function
+    will be converted to json unless force_json is set to False.
+    """
+    kwargs.setdefault('strict_slashes', False)
+    force_json = kwargs.pop('force_json', True)
+    output_schema = kwargs.pop('output_schema', None)
+
+    def bp_router(f):
+        @bp.route(*args, **kwargs)
+        @wraps(f)
+        def view_func_wrapper(*args, **kwargs):
+
+            sc = 200
+            rv = f(*args, **kwargs)
+            if isinstance(rv, tuple):
+                sc = rv[1]
+                rv = rv[0]
+
+            if output_schema:
+                rv = output_schema.dump(rv).data
+
+            if isinstance(rv, Response):
+                return rv
+            elif force_json:
+                return jsonify(rv), sc
+            else:
+                return rv, sc
+
+        return view_func_wrapper
+
+    return bp_router
--- panda_tracker/views/events.py
+++ panda_tracker/views/events.py
@@ -1,10 +1,12 @@
 from flask import Blueprint
 from flask_jwt import jwt_required, current_identity
 
+from ..core import route
+
 bp = Blueprint('events', __name__, url_prefix='/events')
 
 
-@bp.route('/')
+@route(bp, '/')
 @jwt_required()
 def index():
-    return 'Привет, {}!'.format(current_identity)
+    return {'msg': 'Привет, {}!'.format(current_identity)}
