4110d02fe94bb80551c3d53bec18e07e9db9e5ae
Andrey Petrov <nomad@yandex-team.ru>
2016-05-02 01:38:48 +0300

    'events' endpoint is now capable of receiving and storing new data.

--- panda_tracker/views/events.py
+++ panda_tracker/views/events.py
@@ -1,12 +1,44 @@
-from flask import Blueprint
+from datetime import datetime, time
+from flask import Blueprint, request
 from flask_jwt import jwt_required, current_identity
+from bson import ObjectId
+from dateutil.parser import parse
 
-from ..core import route
+from ..core import route, mongo
 
 bp = Blueprint('events', __name__, url_prefix='/events')
 
 
-@route(bp, '/')
+def prepare_update(tick, prefix=''):
+    keys = ('steps', 'distance', 'calories', 'active_time',)
+    return {prefix+key: tick.get(key, 0) for key in keys}
+
+
+@route(bp, '/', methods=('POST',))
 @jwt_required()
-def index():
-    return {'msg': 'Привет, {}!'.format(current_identity)}
+def store_event():
+    ticks = request.get_json()
+
+    for tick in ticks:
+        dt_utc = parse(tick['time_completed'])
+        id_daily = dt_utc.strftime('%Y%m%d.') + str(current_identity)
+        d = datetime.combine(dt_utc.date(), time.min)
+
+        query = {
+            '_id': id_daily,
+            'metadata': {
+                'date': d, 'userid': ObjectId(str(current_identity))
+            }
+        }
+        activity_prefix = 'activities.{}.'.format(tick['activity_kind'])
+
+        inc = {}
+        # total among all activities
+        inc.update(prepare_update(tick))
+        # total for current activity
+        inc.update(prepare_update(tick, activity_prefix))
+
+        update = {'$inc': inc}
+        mongo.db.event_data.update_one(query, update, upsert=True)
+
+    return ''
---
+++ panda_tracker/views/stats.py
@@ -0,0 +1,63 @@
+from webargs import fields, missing
+from datetime import datetime, date, time, timedelta
+from flask import Blueprint, request
+from flask_jwt import jwt_required, current_identity
+from webargs.flaskparser import use_kwargs
+
+from bson import ObjectId
+
+from ..core import route, mongo
+
+bp = Blueprint('stats', __name__, url_prefix='/stats')
+
+stat_args = {
+    'date_from': fields.Date(required=False, load_from='from'),
+    'date_to': fields.Date(required=False, load_from='to'),
+
+    'last_n_days': fields.Int(required=False),
+}
+
+
+def validate(args):
+    return (args.get('date_from') is not missing or
+            args.get('last_n_days') is not missing)
+
+
+@route(bp, '/')
+@jwt_required()
+@use_kwargs(stat_args, locations=('query',), validate=validate)
+def get_stats(date_from, date_to, last_n_days):
+    """Returns statistics over specified period.
+    That period can be defined in two ways:
+        1. In the form of 'Last N days'
+        2. More usual form with start date and end date. End date defaults to
+        today.
+    """
+    if last_n_days is not missing:
+        date_from = date.today() - timedelta(days=last_n_days)
+
+    if date_to is missing:
+        date_to = date.today()
+
+    # convert dates to datetimes
+    date_from = datetime.combine(date_from, time.min)
+    date_to = datetime.combine(date_to, time.min)
+
+    print({
+            'metadata.userid': ObjectId(str(current_identity)),
+            'metadata.date': {'$gte': date_from, '$lte': date_to},
+        })
+
+    stats = mongo.db.event_data.find(
+        {
+            'metadata.userid': ObjectId(str(current_identity)),
+            'metadata.date': {'$gte': date_from, '$lte': date_to},
+        },
+        # exclude meta fields from the output
+        projection={'metadata.userid': 0, '_id': 0},
+        sort=[('metadata.date', 1)]
+    )
+
+    rv = list(stats)
+    # print(rv)
+    return rv
